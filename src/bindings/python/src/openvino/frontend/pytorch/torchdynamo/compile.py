a6422f2e1136b7e742a047222a8271bf8c3f5c1e
Yamini Nimmagadda <yamini.nimmagadda@intel.com>
2023-11-14 22:37:35 -0800

    Add FP32 precision in compile model for GPU training

--- src/bindings/python/src/openvino/frontend/pytorch/torchdynamo/compile.py
+++ src/bindings/python/src/openvino/frontend/pytorch/torchdynamo/compile.py
@@ -130,6 +130,7 @@ def openvino_compile(gm: GraphModule, *args, model_hash_str: str = None):
 
     if model_hash_str is not None:
         core.set_property({'CACHE_DIR': cache_root + '/blob'})
-
-    compiled = core.compile_model(om, device)
+    
+    device_config = {"INFERENCE_PRECISION_HINT": "f32"}
+    compiled = core.compile_model(om, device, device_config)
     return compiled
